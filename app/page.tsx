08b60a1f9ddda4b1b5d6b66494e0379d1fafd170
zahra-55-wq <roojiabuubakar9@gmail.com>
2026-04-14 09:46:21 +0300

    Refactor Home component layout for improved styling and structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
 import ChooseUsSection from "./components/ChooseUsSection";
-
-export default function Home() {
-  return (
-    <main>
-      <ChooseUsSection />
 import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
+
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Hero />
+      <ChooseUsSection />
       {/* Additional sections can be added here */}
     </main>
   );
